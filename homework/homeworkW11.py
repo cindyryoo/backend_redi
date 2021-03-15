6788ff2c0b883433a44a8fc37dcc13455ced3ec9
cindyryoo <cindyryoo1@gmail.com>
2021-03-15 23:29:00 +0100

    quiz1

---
+++ homework/homeworkW11.py
@@ -0,0 +1,18 @@
+
+def validate():
+    while True:
+        password = input("Please enter password: ")
+        if len(password) < 8:
+            print("must have a minimum of 8 characters")
+        elif not any(p.isupper() for p in password):
+            print("must contain at least one upper-case letter ")
+        elif sum(p.isdigit() for p in password) < 2:
+            print("must contain at least two digits")
+        # elif :
+        #     print("Can’t contain two or more consecutive digits")
+        else:
+            print("This is a valid password")
+            break
+
+
+validate()
